d60c498c24c747d4d24762793c8ec1488244f8a7
Francesco De Carlo <decarlof@gmail.com>
2022-02-09 00:41:47 -0700

    testing new hdf parsing

--- mosaic/fileio.py
+++ mosaic/fileio.py
@@ -83,6 +83,7 @@ def extract_meta(fname):
 def extract_dict(fname, list_to_extract, index=0):
 
     meta = dxreader.read_dx_meta(fname, label1='/measurement/instrument/sample_motor_stack/setup/', label2='/measurement/') 
+    # tree, meta = read_dx_meta_new(fname)
     sub_dict = {fname : meta}
 
     return sub_dict
@@ -185,3 +186,135 @@ def tile(args):
 
     # print(f'{x_shift=},{y_shift=}')
     return tile_dict, grid, data_shape, x_shift, y_shift
+
+
+
+# #####################################################################################
+def _get_subgroups(hdf_object, key=None):
+    """
+    Supplementary method for building the tree view of a hdf5 file.
+    Return the name of subgroups.
+    """
+    list_group = []
+    if key is None:
+        for group in hdf_object.keys():
+            list_group.append(group)
+        if len(list_group) == 1:
+            key = list_group[0]
+        else:
+            key = ""
+    else:
+        if key in hdf_object:
+            try:
+                obj = hdf_object[key]
+                if isinstance(obj, h5py.Group):
+                    for group in hdf_object[key].keys():
+                        list_group.append(group)
+            except KeyError:
+                pass
+    if len(list_group) > 0:
+        list_group = sorted(list_group)
+    return list_group, key
+
+def _add_branches(tree, meta, hdf_object, key, key1, index, last_index, prefix,
+                  connector, level, add_shape):
+    """
+    Supplementary method for building the tree view of a hdf5 file.
+    Add branches to the tree.
+    """
+    shape = None
+    key_comb = key + "/" + key1
+    if add_shape is True:
+        if key_comb in hdf_object:
+            try:
+                obj = hdf_object[key_comb]
+                if isinstance(obj, h5py.Dataset):
+                    shape = str(obj.shape)
+                    if obj.shape[0]==1:
+                        s = obj.name.split('/')
+                        # name = s[-1].replace('-', '_')
+                        name = "_".join(s)[1:]
+                        print(s)
+                        print(name)
+                        value = obj[()][0]
+                        attr = obj.attrs.get('units')
+                        if attr != None and args.tree == False:
+                            attr = attr.decode('UTF-8')
+                            log.info(">>>>>> %s <<<<<<<<: %s %s" % (obj.name, value, attr))
+                        if  (value.dtype.kind == 'S') and args.tree == False:
+                            value = value.decode(encoding="utf-8")
+                            log.info("%s: %s" % (obj.name, value))
+                        meta.update( {name : [value, attr] } )
+            except KeyError:
+                shape = str("-> ???External-link???")
+    if shape is not None:
+        tree.append(f"{prefix}{connector} {key1} {shape}")
+    else:
+        tree.append(f"{prefix}{connector} {key1}")
+    if index != last_index:
+        prefix += PIPE_PREFIX
+    else:
+        prefix += SPACE_PREFIX
+    _make_tree_body(args, tree, meta, hdf_object, prefix=prefix, key=key_comb,
+                    level=level, add_shape=add_shape)
+
+def _make_tree_body(args, tree, meta, hdf_object, prefix="", key=None, level=0,
+                    add_shape=True):
+    """
+    Supplementary method for building the tree view of a hdf5 file.
+    Create the tree body.
+    """
+    entries, key = _get_subgroups(hdf_object, key)
+    num_ent = len(entries)
+    last_index = num_ent - 1
+    level = level + 1
+    if num_ent > 0:
+        if last_index == 0:
+            key = "" if level == 1 else key
+            if num_ent > 1:
+                connector = PIPE
+            else:
+                connector = ELBOW if level > 1 else ""
+            _add_branches(tree, meta, hdf_object, key, entries[0], 0, 0, prefix,
+                          connector, level, add_shape)
+        else:
+            for index, key1 in enumerate(entries):
+                connector = ELBOW if index == last_index else TEE
+                if index == 0:
+                    tree.append(prefix + PIPE)
+                _add_branches(tree, meta, hdf_object, key, key1, index, last_index,
+                              prefix, connector, level, add_shape)
+
+def read_dx_meta_new(fname, output=None, add_shape=True):
+    """
+    Get the tree view of a hdf/nxs file.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the file.
+    output : str or None
+        Path to the output file in a text-format file (.txt, .md,...).
+    add_shape : bool
+        Including the shape of a dataset to the tree if True.
+
+    Returns
+    -------
+    list of string
+    """
+    file_path = fname
+    hdf_object = h5py.File(file_path, 'r')
+    tree = deque()
+    meta = {}
+    _make_tree_body(args, tree, meta, hdf_object, add_shape=add_shape)
+    if output is not None:
+        make_folder(output)
+        output_file = open(output, mode="w", encoding="UTF-8")
+        with output_file as stream:
+            for entry in tree:
+                print(entry, file=stream)
+    else:
+        if args.tree:
+            for entry in tree:
+                print(entry)
+    return tree, meta
